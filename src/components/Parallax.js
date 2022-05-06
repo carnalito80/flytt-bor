02eb3957948745c1331be9bec328ff5ff2aa05dc
carnalito80 <kunbg_jk@hotmail.com>
2022-05-06 14:04:55 +0200

    priser

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -18,27 +18,27 @@ export default () => (
           <ul>        
           
             <li> <span className="Left">0 - 49 kvm </span><span className="Separator"></span>
-            <span className="Right">1200 kronor</span></li>
+            <span className="Right">1600 kronor</span></li>
             <li> 50 - 59 kvm<span className="Separator"></span>
-            1400 kronor</li>
-            <li> 60 - 69 kvm<span className="Separator"></span>
-            1600 kronor</li>
-            <li> 70 - 79 kvm<span className="Separator"></span>
             1800 kronor</li>
-            <li> 80 - 89 kvm<span className="Separator"></span>
+            <li> 60 - 69 kvm<span className="Separator"></span>
             2000 kronor</li>
-            <li> 90 - 99 kvm<span className="Separator"></span>
+            <li> 70 - 79 kvm<span className="Separator"></span>
             2200 kronor</li>
-            <li> 100 - 114kvm<span className="Separator"></span>
+            <li> 80 - 89 kvm<span className="Separator"></span>
             2400 kronor</li>
-            <li> 115 - 124kvm<span className="Separator"></span>
+            <li> 90 - 99 kvm<span className="Separator"></span>
             2600 kronor</li>
-            <li> 125 - 136kvm<span className="Separator"></span>
+            <li> 100 - 114kvm<span className="Separator"></span>
             2800 kronor</li>
-            <li> 137 - 148 kvm<span className="Separator"></span>
+            <li> 115 - 124kvm<span className="Separator"></span>
             3000 kronor</li>
-            <li> 149 - 159 kvm<span className="Separator"></span>
+            <li> 125 - 136kvm<span className="Separator"></span>
             3200 kronor</li>
+            <li> 137 - 148 kvm<span className="Separator"></span>
+            3400 kronor</li>
+            <li> 149 - 159 kvm<span className="Separator"></span>
+            3600 kronor</li>
             <li> 
             Över 159 kvm<span className="Separator"></span>
             begär offert</li>
@@ -50,8 +50,8 @@ export default () => (
 
         Balkong: 300 kronor<br></br>
         Inglasad balkong: 400 kronor<br></br>
-        Vind (i hyreshus): 300 kronor<br></br>
-        Källare i hyreshus: 300 kronor<br></br>
+        Vind (i hyreshus): 400 kronor<br></br>
+        Källare i hyreshus: 400 kronor<br></br>
         All städutrustning och allt material tillhandahålls självklart av oss på Flyttstädning Borås och ingår i priset.
         Vi debiterar inte för transportkostnader.
         </p>
